b33a3515fa26e7d468f4fdd5d853e8b1c4c25840
stefanieradnik <96581553+stefanieradnik@users.noreply.github.com>
2026-05-17 22:01:24 +0200

    Fix branch (#16)
    * minor fix
    
    * add .db to repo
    
    * fix pyproject.toml
    
    * fix pyproject.toml

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,4 @@
 import yaml
-from pathlib import Path
 import streamlit as st
 from ui_utils import (
     get_unique_bundeslaender,
@@ -8,23 +7,15 @@ from ui_utils import (
     get_text_from_id,
 )
 from comparer import FuzzyComparer
-from data_pipeline import DataPipeline
 
 st.title("BSBE Bundesländer Vergleich 😊")
 
 st.header("Referenz 📌:")
 
-with open("config.yaml", "r") as f:
+with open("src/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 db_path = config["db_path"]
-
-# set db
-pfad = Path(db_path)
-
-if not pfad.exists():
-    pipeline = DataPipeline(config)
-    pipeline.run()
 
 # st.session_state()
 bundesland_options = get_unique_bundeslaender(db_path)
